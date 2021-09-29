382a9735e18fd05bbd4eeb84daff54fac6e5f971
Ajeydixit <67639919+Ajeydixit@users.noreply.github.com>
2021-09-29 21:44:29 +0530

    Add files via upload

---
+++ Assets/Bird.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour {
+
+    Vector3 _initialPosition;
+    [SerializeField] private float _launchscale =500 ;
+
+    private bool _birdWasLaunch;
+    private float timeSittingAround;
+
+    private void Awake () {
+        _initialPosition = transform.position;
+
+    }
+
+    private void Update()
+    {
+        GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+
+
+        if (_birdWasLaunch && 
+            GetComponent<Rigidbody2D>().velocity.magnitude <=0.1)
+        {
+            timeSittingAround += Time.deltaTime;
+        }
+        if (transform.position.y > 10 || transform.position.x > 14 || transform.position.y < -10 || transform.position.x < -20 || timeSittingAround > 3)
+        {   
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnMouseDown () {
+        GetComponent<SpriteRenderer> ().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true;
+    }
+
+    private void OnMouseUp () {
+        GetComponent<SpriteRenderer> ().color = Color.white;
+        Vector2 directonToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directonToInitialPosition * _launchscale);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLaunch = true;
+        GetComponent<LineRenderer>().enabled = false;
+
+
+    }
+
+    private void OnMouseDrag () {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        transform.position = new Vector3 (newPosition.x, newPosition.y);
+    }
+
+}
---
+++ Assets/enemy/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject particleSystemPrefab;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.GetComponent<Bird>() != null )
+        {
+            Instantiate(particleSystemPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+            return;
+        }
+
+        if(collision.collider.GetComponent<Enemy>() != null)
+        {
+            return;
+        }
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            Instantiate(particleSystemPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+
+    }
+}
